cff00f06c395d1edde803f0c70c923888f1422ac
Rahul Rana <rahulrana@Rahuls-MacBook-Pro.local>
2024-12-06 12:46:31 +0530

    test: automate workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import "./App.css";
 import { TodoWrapper } from "./components/TodoWrapper";
 function App() {
+  useEffect(() => {
+    for (let index = 0; index < 1000; index++) {
+      console.log(index);
+    }
+  }, []);
   return (
     <>
       <div className="App">
